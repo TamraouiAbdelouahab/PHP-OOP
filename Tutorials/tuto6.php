7db9264e678fca1210fa60d8f4bb42c66f15081c
TamraouiAbdelouahab <abdelwahabtamraoui@gmail.com>
2024-09-19 16:56:08 +0100

    Ajouter tuto6

---
+++ Tutorials/tuto6.php
@@ -0,0 +1,38 @@
+<?php
+// Récupération des deux nombres
+$nombre1 = readline("Entrez le premier nombre : ");
+
+$nombre2 = readline("Entrez le deuxième nombre : ");
+
+// Récupération de l'opération
+echo "Choisissez l'opération (+, -, *, /) : ";
+$operation = readline("Operation : ");
+
+// Calcul en fonction de l'opération
+switch ($operation) {
+    case "+":
+        $resultat = $nombre1 + $nombre2;
+        break;
+    case "-":
+        $resultat = $nombre1 - $nombre2;
+        break;
+    case "*":
+        $resultat = $nombre1 * $nombre2;
+        break;
+    case "/":
+        if ($nombre2 == 0) {
+            echo "Division par zéro impossible !\n";
+        } else {
+            $resultat = $nombre1 / $nombre2;
+        }
+        break;
+    default:
+        echo "Opération invalide.\n";
+}
+
+// Affichage du résultat
+if (isset($resultat)) {
+    echo "Le résultat est : " . $resultat . "\n";
+}
+
+?>
